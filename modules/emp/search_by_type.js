614a9601027841677abd8e90cba931d6aa73c2b8
weita0 <weitao907@gmail.com>
2016-05-03 22:24:43 +0800

    update search_by_type;

--- modules/emp/search_by_type.js
+++ modules/emp/search_by_type.js
@@ -10,17 +10,25 @@ var searchByType = function (type, fn) {
 		var cursor = db.collection('employee').find({ 'type': type });
 		var res = [];
 		cursor.forEach(function (doc) {
-			var imageurl = doc.imageurl;
+			var imageurl = doc.imageurl.replace('\\', '\/');
+			
 			if (imageurl !== undefined) {
-				var image = path.join(host, imageurl);
+				var image = host + '\/' + imageurl;
 			}
+			if (doc.score === undefined) {
+				var score = 0;
+			} else {
+				var score = doc.score;
+			}
+			
+			logger.debug('imageurl =>', imageurl);
 			res.push({
 				name: doc.name,
 				workid: doc.workid,
 				gender: doc.gender,
 				age: age(doc.birth),
 				locate: doc.locate,
-				score: doc.score,
+				score: score,
 				imageurl: image
 			});
 		}, function (err) {
